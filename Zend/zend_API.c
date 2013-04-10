69a4c188aadf6937a9841ef96c9dab717918c003
Julien Pauli <jpauli@php.net>
2013-04-10 17:54:10 +0200

    Better error messages and Zend/PHP extension mismatch detection

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2381,7 +2381,10 @@ void module_destructor(zend_module_entry *module) /* {{{ */
 #if HAVE_LIBDL
 #if !(defined(NETWARE) && defined(APACHE_1_BUILD))
 	if (module->handle && !getenv("ZEND_DONT_UNLOAD_MODULES")) {
-		DL_UNLOAD(module->handle);
+#if ZEND_DEBUG
+	fprintf(stderr, "Unloaded extension '%s', version %s\n", module->name, module->version);
+#endif
+	DL_UNLOAD(module->handle);
 	}
 #endif
 #endif
